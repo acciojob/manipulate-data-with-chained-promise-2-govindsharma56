8ce4b5b2e6956744cea2de4512351a2a5e3ad0a4
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-09-25 23:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let output=document.querySelector('#output');
+let arr=[1,2,3,4];
+function manipulateArray(arr) {
+  return Promise.resolve(arr)
+    .then((data) => {
+        // 1. Odd numbers remove
+        return data.filter(num => num % 2 === 0);
+    })
+    .then((data) => {
+        // 2. Even numbers × 2
+        return data.map(num => num * 2);
+    });
+}
+manipulateArray([1, 2, 3, 4]) 
+.then((result) => 
+{ document.querySelector("#output").innerText = "Result: " + result.join(", ");
+});
