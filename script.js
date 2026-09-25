d1f4caae56209d8424cd2e7a8f6a8337a88f4bac
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-09-25 23:40:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-
-// your JS code here. If required.
-
 let output = document.querySelector('#output');
 let arr = [1, 2, 3, 4];
-
 function manipulateArray(arr) {
 
-    return Promise.resolve(arr)
+    // Initial promise resolves after 3 seconds
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(arr);
+        }, 3000);
+    })
 
-        // First transformation: filter even numbers
-        .then((data) => {
-            return new Promise((resolve) => {
-                setTimeout(() => {
+    // First transformation after initial promise
+    .then((data) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
 
-                    let evenNumbers = data.filter(num => num % 2 === 0);
+                let evenNumbers = data.filter(num => num % 2 === 0);
 
-                    output.innerText = JSON.stringify(evenNumbers);
+                output.innerText = JSON.stringify(evenNumbers);
 
-                    resolve(evenNumbers);
+                resolve(evenNumbers);
 
-                }, 3000);
-            });
-        })
-
-        // Second transformation: multiply by 2
-        .then((data) => {
-            return new Promise((resolve) => {
-                setTimeout(() => {
-
-                    let result = data.map(num => num * 2);
-
-                    output.innerText = JSON.stringify(result);
-
-                    resolve(result);
-
-                }, 2000);
-            });
+            }, 1000);
         });
+    })
+
+    // Second transformation
+    .then((data) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+
+                let result = data.map(num => num * 2);
+
+                output.innerText = JSON.stringify(result);
+
+                resolve(result);
+
+            }, 2000);
+        });
+    });
 }
 
 manipulateArray(arr);
+
